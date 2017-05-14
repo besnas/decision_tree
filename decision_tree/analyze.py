389a8546ef504e1d3de64f92adf311a3ad4fb011
besnas <bestyzheva@gmail.com>
2017-05-15 00:01:41 +0400

    test
    test

--- decision_tree/analyze.py
+++ decision_tree/analyze.py
@@ -20,11 +20,13 @@ class Analyzer:
 			(tr, te) = self.samples.random_split()
 			samples.append((tr, te))
 			length = len(tr)
+			length2 = len(te)
+			print length2
 		
 		step = self.step(length)
 		
 		"""calculate averages"""
-		counts, node_counts, averages, average_nodes, total = {}, {}, {}, {}, 0
+		counts, node_counts, averages, average_nodes, per, total = {}, {}, {}, {}, {}, 0
 		for i in step:
 			print i
 			for sample in samples:
@@ -37,12 +39,14 @@ class Analyzer:
 				counts[i] += self.test(tree, te)
 				node_counts[i] += tree.number_of_nodes()
 				total = len(te)
+				#print counts
 			averages[i] = counts[i] / float(len(samples) * total)
 			average_nodes[i] = node_counts[i] / float(len(samples))
-			
-			self.write(averages, average_nodes)
+			per[i] = self.test(tree, te) / float(length2)
+			print  per[i]
+			self.write(averages, average_nodes, per)
 	
-	def write(self, ave, node_ave):
+	def write(self, ave, node_ave, per):
 		"""outputs averages"""
 		file_name = "output/correct_averages_%s" % (self.file_name)
 		if self.prune:
@@ -57,6 +61,11 @@ class Analyzer:
 		w = csv.writer(open(file_name, 'wb'), dialect='excel')
 		for k in sorted(node_ave.keys()):
 			w.writerow([k, node_ave[k]])
+
+		file_name = "output/percents_%s" % (self.file_name)
+		w = csv.writer(open(file_name, 'wb'), dialect='excel')
+		for k in sorted(per.keys()):
+			w.writerow([ per[k]])
 					
 	def test(self, tree, tests):
 		"""compares expected with actual values"""
@@ -69,9 +78,10 @@ class Analyzer:
 	def step(self, length):
 		"""generates the steps to iterate over"""
 		"""does every 5 starting at 5, 1, and n if its not a multiple of 5"""
-		step = [i for i in range(5, length, 5)]
+		step = [i for i in range(50, length, 50)]
 		step.insert(0, 1)
-		if length % 5 != 0:
+		if length % 50 != 0:
 			step.append(length)
+		print step
 		return step
 			
--- decision_tree/data.py
+++ decision_tree/data.py
@@ -9,11 +9,12 @@ class Samples:
 		for row in csv.reader(open(file_name, 'rb')):
 			self.data.append(row)
 			for i in range(len(row) - 1):
-				if self.attrs.get(i):
-					if not row[i] in self.attrs[i]:
+				if self.attrs.get(i):        			#if attr exists
+					if not row[i] in self.attrs[i]:		#if there's no such attr
 						self.attrs[i].append(row[i])
 				else:
 					self.attrs[i] = [row[i]]
+		print self.attrs
 				
 	def attributes(self):
 		"""attributes linked to their values"""
@@ -22,5 +23,5 @@ class Samples:
 	def random_split(self):
 		"""creates random splits of the data"""
 		random.shuffle(self.data)
-		middle = len(self.data) / 2
+		middle = 3*len(self.data) / 4
 		return (self.data[0:middle], self.data[middle:])
